7562a652ee621ff40ca3e47792e649728bab8d2f
Danielmontesgil <dmontesgil@gmail.com>
2019-01-25 21:23:17 -0500

    ScriptableObjects

---
+++ Assets/Scripts/ScriptableObject/PlayerScriptableObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewPlayer", menuName = "Player")]
+public class PlayerScriptableObject : ScriptableObject
+{
+    public string name;
+    public Sprite image;
+    public int health;
+    public int maxHealth;
+    public int baseAttack;
+    public int attack;
+    public int baseMovementSpeed;
+    public float movementSpeed;
+    public int score;
+    public int deathCount;
+}
